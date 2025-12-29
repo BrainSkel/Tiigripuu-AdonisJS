ca82a612adc55ee0d1ca8633f34bb1533fa1432f
BrainSkel <hanne53rik@gmail.com>
2025-12-29 13:47:31 +0200

    added rentals controller

--- start/routes.ts
+++ start/routes.ts
@@ -6,7 +6,11 @@
 | The routes file is used for defining the HTTP routes.
 |
 */
-
+import RentalsController from '#controllers/rentals_controller';
 import router from '@adonisjs/core/services/router'
 
 router.on('/').render('pages/home')
+
+router.resource('rentals', RentalsController).params({
+    rentals: 'slug',
+})
