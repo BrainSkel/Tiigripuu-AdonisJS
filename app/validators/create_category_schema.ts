2c1880c686725110dc84bdc743734356905c6a3e
BrainSkel <hanne53rik@gmail.com>
2026-01-22 00:50:21 +0200

    tofix: move category creation to one form. Also fixed more category stuff

--- app/validators/create_category_schema.ts
+++ app/validators/create_category_schema.ts
@@ -2,14 +2,16 @@
 import vine from '@vinejs/vine'
 
 export const createCategorySchema = vine.compile(vine.object({
+    product_type: vine.enum(['handicraft', 'rental', undefined]),
     name: vine.string().unique(async (db, value, field) => {
+        const product_type = await field.data?.product_type
         //const id = field.meta?.id
         const existingCategory = await db
         .from('categories')
+        .where('product_type', product_type)
         .where('name', value)
         .first()
         return !existingCategory
     }),
-    product_type: vine.enum(['handicraft', 'rental', undefined]),
 
 }));
