9e8120662ab1f24c1bfb2fa320ba51b1dce00618
BrainSkel <hanne53rik@gmail.com>
2026-02-27 23:31:39 +0200

    fixed pricing with decimals not working correctly

--- app/validators/create_product_schema.ts
+++ app/validators/create_product_schema.ts
@@ -2,7 +2,7 @@ import vine from '@vinejs/vine'
 
 export const createProductSchema = vine.compile(vine.object({
     item_name: vine.string().maxLength(255),
-    price: vine.number().decimal(2).positive(),
+    price: vine.number().positive().decimal([0,2]),
     description: vine.string().maxLength(1000),
     stock_amount: vine.number().positive(),
     //is_visible: vine.boolean(),
