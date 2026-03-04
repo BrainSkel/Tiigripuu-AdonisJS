0a4cab0182dcfd3b0e3654a478599b6381eaac32
BrainSkel <hanne53rik@gmail.com>
2026-03-04 23:46:35 +0200

    got loging in and loging out working

--- app/controllers/session_controller.ts
+++ app/controllers/session_controller.ts
@@ -3,15 +3,17 @@ import User from '#models/user'
 
 export default class SessionController {
 
-    async login({view}:HttpContext) {
+    async index({ view }: HttpContext) {
 
         return view.render('auth/login')
     }
 
-    async store({ request, auth, response}: HttpContext) {
-        const {email, password} = request.only(['email', 'password'])
+    async store({ request, auth, response }: HttpContext) {
+        console.log('start')
+        const { email, password } = await request.only(['email', 'password'])
+        console.log(email, password)
         const user = await User.verifyCredentials(email, password);
-console.log('1')
+        console.log('1')
         await auth.use('web').login(user, !!request.input('remember_me'))
         console.log('logged in')
 
@@ -19,9 +21,9 @@ console.log('1')
         return response.redirect().toRoute('admin.dashboard')
     }
 
-    async destroy({ auth, response} :HttpContext) {
+    async destroy({ auth, response }: HttpContext) {
         await auth.use('web').logout()
-        return response.redirect('home')
+        return response.redirect().toRoute('home')
     }
 
 }
