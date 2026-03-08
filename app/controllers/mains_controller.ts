c6564265d7348c46930ef860819a200494faf8db
BrainSkel <hanne53rik@gmail.com>
2026-03-08 19:42:03 +0200

    fixed displaying non active products

--- app/controllers/mains_controller.ts
+++ app/controllers/mains_controller.ts
@@ -3,8 +3,8 @@ import type { HttpContext } from '@adonisjs/core/http'
 
 export default class MainsController {
     async index( {view} :HttpContext ){
-        const rentals = await Product.query().where('product_type', 'rental').preload('images').preload('rentalDetail').preload('categories')
-        const handicrafts = await Product.query().where('product_type', 'handicraft').preload('images').preload('handicraftDetail').preload('categories')
+        const rentals = await Product.query().where('product_type', 'rental').where('is_active', true).preload('images').preload('rentalDetail').preload('categories')
+        const handicrafts = await Product.query().where('product_type', 'handicraft').where('is_active', true).preload('images').preload('handicraftDetail').preload('categories')
 
         return view.render('pages/home', {rentals: rentals, handicrafts: handicrafts})
 
